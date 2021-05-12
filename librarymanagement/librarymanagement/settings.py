ecd4f9792ff54c7e584a54d85e6cdc855d317f42
New-Byte <64476132+New-Byte@users.noreply.github.com>
2021-05-12 17:21:27 +0530

    Update settings.py

--- librarymanagement/librarymanagement/settings.py
+++ librarymanagement/librarymanagement/settings.py
@@ -131,5 +131,5 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'prasadjoshi7767@gmail.com'
-EMAIL_HOST_PASSWORD = 'Durvankur'
+EMAIL_HOST_USER = '<Your Email>'
+EMAIL_HOST_PASSWORD = '<your password>'
